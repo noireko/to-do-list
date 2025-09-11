c39d8d567b9847a04038e9767cfdea4361ec6932
noireko <162654411+noireko@users.noreply.github.com>
2025-09-11 20:13:53 -0300

    Added a max task per page, added navegation functionality

--- js.js
+++ js.js
@@ -6,52 +6,77 @@ const descInput = document.getElementById("descInput")
 
 let listaTareas = JSON.parse(localStorage.getItem("listaTareas")) || [];
 
+const maxPorHoja = 3;
+let currentPage = 0; 
 
 function agregarTarea() {
 
-    const tarea = {
-        titulo: tituInput.value,
-        fecha: fechaInput.value,
-        descripcion: descInput.value,
-    }
+  const tarea = {
+    titulo: tituInput.value,
+    fecha: fechaInput.value,
+    descripcion: descInput.value,
+  }
 
-    
+  listaTareas.push(tarea);
+  localStorage.setItem("listaTareas", JSON.stringify(listaTareas));
 
-    listaTareas.push(tarea);
-    localStorage.setItem("listaTareas", JSON.stringify(listaTareas));
+  tituInput.value = "";
+  fechaInput.value = "";
+  descInput.value = "";
 
-    tituInput.value = "";
-    fechaInput.value = "";
-    descInput.value = "";
-
-    window.location.href = "tareas.html";
+  window.location.href = "tareas.html";
 }
 
 const mostrarTarea = () => {
-    sectTareas.innerHTML = "";
-    listaTareas.forEach((tarea, index) => {
-        sectTareas.innerHTML += `
+  sectTareas.innerHTML = "";
+  const start = currentPage * maxPorHoja;
+  const end = start + maxPorHoja;
+  const tareasPagina = listaTareas.slice(start, end);
+  tareasPagina.forEach((tarea, index) => {
+    sectTareas.innerHTML += `
       <div class="tarea-item">
         <h2 class="titulo-tarea">${tarea.titulo}</h2>
         <p class="fecha-tarea">${tarea.fecha}</p>
         <p class="des-tarea">${tarea.descripcion}</p>
-        <button class="boton-elimtarea" onclick="eliminarTarea(${index})">Eliminar</button>
+        <button class="boton-elimtarea" onclick="eliminarTarea(${start + index})">Eliminar</button>
       </div>
     `;
-    });
-};
+  });
+}
 
 if (sectTareas) {
   mostrarTarea();
 }
 
+
 const eliminarTarea = (index) => {
   listaTareas.splice(index, 1);
   localStorage.setItem("listaTareas", JSON.stringify(listaTareas));
+
+  if (currentPage > 0 && currentPage * maxPorHoja >= listaTareas.length) {
+    currentPage--;
+  }
   mostrarTarea();
-};
+}
 
 window.eliminarTarea = eliminarTarea;
 
-btnAdd.addEventListener("click", agregarTarea)
+btnAdd.addEventListener("click", agregarTarea);
+
+function paginaSiguiente() {
+  if ((currentPage + 1) * maxPorHoja < listaTareas.length) {
+    currentPage++;
+    mostrarTarea();
+  }
+}
+
+function paginaAnterior() {
+  if (currentPage > 0) {
+    currentPage--;
+    mostrarTarea();
+  }
+}
+
+window.paginaSiguiente = paginaSiguiente;
+window.paginaAnterior = paginaAnterior;
 
